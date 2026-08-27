e80ed528d230329a926572f4365d86ed87add823
Dazinha <pablo.daza.g@mail.pucv.cl>
2026-08-26 22:24:00 -0400

    Corregir posición del logo hacia abajo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           backgroundImage: 'url(/CERBERUSTECHLOGO.png)',
           backgroundSize: 'max(600px, 60vw)',
           backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center 60%',
+          backgroundPosition: 'center calc(50% + 100px)',
           zIndex: -1, opacity: 0.15
         }}></div>
         {/* Background Glow */}
