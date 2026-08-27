476356e29ace67273e314e42f93d19216a5378da
Dazinha <pablo.daza.g@mail.pucv.cl>
2026-08-26 22:22:35 -0400

    Bajar la posición del logo de fondo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           backgroundImage: 'url(/CERBERUSTECHLOGO.png)',
           backgroundSize: 'max(600px, 60vw)',
           backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
+          backgroundPosition: 'center 60%',
           zIndex: -1, opacity: 0.15
         }}></div>
         {/* Background Glow */}
